212fb29dbeb040299f614d107a68da2774e1f100
DenisB7 <denzelbe1987@gmail.com>
2023-07-12 23:58:32 +0500

    Fix names

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function Board({ history, onUserCircleClick, onUserResetClick, onUserConfirmClic
     "#ff0000": "#FFFFFF",
   };
 
-  function handleUserClickOnCircles(circle) {
+  function handleUserClickOnCircle(circle) {
     let checkNumberOfEnteredColors = history.filter(color => color !== "#FFFFFF");
     let userIsChangingTheSameCircle = history.filter((color, index) => color !== "#FFFFFF" && index === circle);
     if (checkNumberOfEnteredColors.length !== 4 || userIsChangingTheSameCircle.length === 1) {
       let updatedHistory = history.slice();
       updatedHistory[circle] = handleColors[history[circle]];
-      onUserCircleClick(circle, updatedHistory);
+      onUserCircleClick(updatedHistory);
       if (document.getElementById("message-to-user").textContent !== "Great! Please enter your PINColor code and click CONFIRM button!") {
         document.getElementById("message-to-user").textContent = "Choose 4 colors by clicking on circles, remember it and SAVE!";
       }
@@ -45,7 +45,7 @@ function Board({ history, onUserCircleClick, onUserResetClick, onUserConfirmClic
       circlesBoard.length = 0;
     }
     let style = {backgroundColor: color};
-    circlesBoard.push(<CircleButton key={index} className="circle" style={style} onUserCircleClick={() => handleUserClickOnCircles(index)} />)
+    circlesBoard.push(<CircleButton key={index} className="circle" style={style} onUserCircleClick={() => handleUserClickOnCircle(index)} />)
     row++
   });
 
@@ -67,7 +67,11 @@ export default function Pin() {
   const [history, setHistory] = useState(Array(10).fill("#FFFFFF"));
   const [savedPinColor, setSavedPinColor] = useState([]);
 
-  function handleUserClickOnSave() {
+  function handleUserClickOnCircle(updatedHistory) {
+    setHistory(updatedHistory);
+  };
+
+  function handleUserClickOnSaveButton() {
     let chosenColors = history.filter(color => color !== "#FFFFFF");
     if (chosenColors.length === 4 && savedPinColor.length === 0) {
       setSavedPinColor(history);
@@ -78,17 +82,13 @@ export default function Pin() {
     }
   };
 
-  function handleUserClickOnCircles(circle, updatedHistory) {
-    setHistory(updatedHistory);
-  };
-
-  function handleUserClickOnReset() {
+  function handleUserClickOnResetButton() {
     setHistory(Array(10).fill("#FFFFFF"));
     setSavedPinColor([]);
     document.getElementById("message-to-user").textContent = "Choose 4 colors by clicking on circles, remember it and SAVE!";
   };
 
-  function handleUserClickOnConfirm(updatedHistory) {
+  function handleUserClickOnConfirmButton(updatedHistory) {
     let savedColors = savedPinColor.filter(color => color !== "#FFFFFF");
     let chosenColors = updatedHistory.filter(color => color !== "#FFFFFF");
     if (chosenColors.length === 4 && savedColors.length === 4) {
@@ -105,14 +105,19 @@ export default function Pin() {
     <div className="main-block">
       <h3 id="message-to-user">Choose 4 colors by clicking on circles, remember it and SAVE!</h3>
       <div className="button-save">
-        <button className="save" onClick={handleUserClickOnSave}>save</button>
+        <button className="save" onClick={handleUserClickOnSaveButton}>save</button>
       </div>
       <Board
         history={history}
-        onUserCircleClick={handleUserClickOnCircles}
-        onUserResetClick={handleUserClickOnReset}
-        onUserConfirmClick={handleUserClickOnConfirm}
+        onUserCircleClick={handleUserClickOnCircle}
+        onUserResetClick={handleUserClickOnResetButton}
+        onUserConfirmClick={handleUserClickOnConfirmButton}
       />
     </div>
   );
+}
+
+
+function SaveButton({ history, }) {
+
 }
